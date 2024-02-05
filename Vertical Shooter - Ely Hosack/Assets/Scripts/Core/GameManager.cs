c642bd2aa5bf409445acb816560c92358247d8ba
Elysium Hosack <elysium.hosack@mymail.champlain.edu>
2024-02-05 10:04:43 -0500

    GameManager updates
    total spaghetti... fix?

--- Vertical Shooter - Ely Hosack/Assets/Scripts/Core/GameManager.cs
+++ Vertical Shooter - Ely Hosack/Assets/Scripts/Core/GameManager.cs
@@ -1,14 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Net;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    private const float MaxPlasma = 200f;
     public int score, lives, world = 1, level = 0, shots = 0, hits = 0, health = 10;
-    public bool gameOver;
-    public float progress, accuracy;
+    public bool gameOver = false, playing = false;
+    public float progress, accuracy, plasma;
     public double time;
+
+    public List<Level[]> worlds;
+
 
     public static GameManager singleton;
 
@@ -21,53 +27,193 @@ public class GameManager : MonoBehaviour
     public static float maxX = 2.5f;
     public static float maxY = 1f;
 
+    private GameObject canvas;
+    private GameObject gameOverObject;
+    private GameObject HUD;
+    private GameObject pauseObject;
+
+    private static readonly string GAMEPLAY_SCENE = "SampleScene";
+    private Coroutine plasmaComputer;
+    private Coroutine enemySpawner;
+
+    public MusicHandler musicHandler;
+    private Coroutine timestop;
+
     private void Awake()
     {
-        singleton = this;
+        if (singleton == null)
+        {
+            singleton = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
     }
 
     // Start is called before the first frame update
     void Start()
     {
         tag= "GameManager";
-        DontDestroyOnLoad(gameObject);
+        musicHandler = GetComponent<MusicHandler>();
+        
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(playing)
+        {
+            if(pauseObject != null)
+            {
+                pauseObject.SetActive(false);
+            }
+            if ((health <= 0))
+            {
+                GameOver();
+            }
+            else if (plasma <= 0 && playing)
+            {
+                Finish();
+            }
+            if(Input.GetKeyDown(KeyCode.Escape))
+            {
+                if (playing)
+                {
+                    Pause();
+                }
+                else
+                {
+                    Unpause();
+                }
+            }
+        }
         
+    }
+
+    public void Pause()
+    {
+        if(pauseObject != null)
+        {
+            pauseObject.SetActive(true);
+        }
+        else
+        {
+            Debug.Log(GameObject.FindWithTag("Pause"));
+            pauseObject = GameObject.FindWithTag("Pause");
+            pauseObject.SetActive(true);
+        }
+        Time.timeScale = 0;
+        playing = false;
+    }
+
+    public void Unpause()
+    {
+        pauseObject = GameObject.FindWithTag("Pause");
+        pauseObject.SetActive(false);
+        Time.timeScale = 1;
+        playing = true;
     }
 
     public bool GameOver()
     {
+        pauseObject = null;
+        plasmaComputer = null;
         gameOver = true;
+        //deactivate input
+        //display game over screen
+        HUD = GameObject.Find("HUD");
+        canvas = GameObject.FindGameObjectWithTag("UICanvas");
+        gameOverObject = canvas.transform.Find("GameOver").gameObject;
+        HUD = canvas.transform.Find("HUD").gameObject;
+        gameOverObject.SetActive(true);
+        HUD.SetActive(false);
+        //disconnect canvas from reference
+        musicHandler.StopMusic();
+        
+        //disable player input
+        playing = false;
+        //destroy enemy manager
+        FindAnyObjectByType<EnemyManager>().StopCoroutine("Co_SpawnEnemy");
+
+
 
         return true;
     }
+
+    public void Finish()
+    {
+        plasmaComputer = null;
+        gameOver = true;
+        playing = false;
+        canvas = GameObject.FindGameObjectWithTag("UICanvas");
+        HUD = canvas.transform.Find("HUD").gameObject;
+        HUD.SetActive(false);
+        Level finishedLevel = new(health, shots, hits, 50);
+        playing = false;
+        FindAnyObjectByType<EnemyManager>().StopCoroutine("Co_SpawnEnemy");
+    }
+
+    
+
 
     public bool ResetGame()
     {
         score = 0;
         lives = 3;
         gameOver = false;
-        health = 100;
+        health = 10;
         progress = 0;
         time = 0;
-
+        plasma = MaxPlasma;
         world = 1;
         level = 1;
-
+        if(timestop != null)
+        {
+            StopCoroutine(timestop);
+        }
         return true;
     }
 
     public void StartGame ()
     {
-        time = 0;
+        ResetGame();
+        SceneManager.LoadScene(GAMEPLAY_SCENE);
     }
+
+    
 
     public void Shot()
     {
         shots++;
     }
+
+    public void Restart()
+    {
+        ResetGame();
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void Playing()
+    {
+        plasma = MaxPlasma;
+        playing = true;
+        plasmaComputer = StartCoroutine(DrainPlasma());
+        enemySpawner = FindAnyObjectByType<EnemyManager>().StartCoroutine("Co_SpawnEnemy");
+    }
+    
+
+    IEnumerator DrainPlasma()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(.1f);
+            plasma -= .1f;
+        }
+    }
+
 }
+
+
---
+++ Vertical Shooter - Ely Hosack/Assets/Scripts/Core/Level.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class Level
+{
+    public static int MAX_STARS = 3;
+    public static int MAX_SCORE = 500000;
+    public static int HEALTH_BONUS = 1000;
+    public int finalHealth, finalShots, finalHits, finalScore, stars, targetEnemies;
+    public float accuracy;
+
+    //constructor
+    public Level(int finalHealth, int finalShots, int finalHits, int targetEnemies)
+    {
+        this.finalHealth = finalHealth;
+        this.finalShots = finalShots;
+        this.finalHits = finalHits;
+        this.targetEnemies = targetEnemies;
+        this.accuracy = ((float)finalHits / (float)finalShots);
+
+        this.finalScore = (int)((finalHealth * HEALTH_BONUS) + (accuracy * MAX_SCORE) + ((finalHits / targetEnemies) * 10000));
+        this.stars = (finalScore / (MAX_SCORE / MAX_STARS));
+
+        Debug.Log("Score: " + finalScore + " Stars: " + stars + "\nAccuracy: " + accuracy);
+    }
+}
